f8f5d5b98d671d634dc1b9260145fcdd5aa8d382
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-07-05 20:46:35 +0200

    feat: create route error handler

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
@@ -25,5 +24,14 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+
+  next();
+});
 
 module.exports = app;
