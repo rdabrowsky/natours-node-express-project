7693311c2faa6279297900bda878b8c53a4cb05c
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-07-07 14:15:07 +0200

    feat: install bcrypt and hash passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,12 +26,21 @@ const userSchema = new mongoose.Schema({
       validator: function (val) {
         return val === this.password;
       },
-      message: 'Password must match',
+      message: 'Passwords must match',
     },
   },
   photo: {
     type: String,
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
