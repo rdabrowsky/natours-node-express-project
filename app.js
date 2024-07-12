b44ed2c4fca8197f062effc3333a83151750a3b3
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-07-12 15:49:44 +0200

    fix: typo issue

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
-// Prevent parameter polution
+// Prevent parameter pollution
 app.use(
   hpp({
     whitelist: ['duration'],
