7f40590b62ab29e518938f94a02000da648684cf
Radosław Dąbrowski <radoslaw.dabrowski2000@wp.pl>
2024-07-18 17:05:46 +0200

    feat: add geospatial aggregation calculate - distances

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -8,6 +8,7 @@ const {
   getAll,
 } = require('./handlerFactory');
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 
 const aliasTopTours = (req, res, next) => {
   const queries = {
@@ -30,6 +31,36 @@ const createTour = createOne(Tour);
 const deleteTour = deleteOne(Tour);
 
 const updateTour = updateOne(Tour);
+
+const getToursWithin = asyncHandler(async (req, res, next) => {
+  // '/tours-within/:distance/center/:latlng/unit/:unit'
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400,
+      ),
+    );
+  }
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
+      },
+    },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { data: tours },
+  });
+});
 
 const getTourStats = asyncHandler(async (req, res, next) => {
   const stats = await Tour.aggregate([
@@ -103,6 +134,45 @@ const getMonthlyPlan = asyncHandler(async (req, res, next) => {
   });
 });
 
+const getDistances = asyncHandler(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400,
+      ),
+    );
+  }
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: { data: distances },
+  });
+});
+
 module.exports = {
   getAllTours,
   getTour,
@@ -112,4 +182,6 @@ module.exports = {
   aliasTopTours,
   getTourStats,
   getMonthlyPlan,
+  getToursWithin,
+  getDistances,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,8 @@ const {
   aliasTopTours,
   getTourStats,
   getMonthlyPlan,
+  getToursWithin,
+  getDistances,
 } = require('../controllers/tourControllers');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
@@ -21,6 +23,13 @@ router.route('/tour-stats').get(getTourStats);
 router
   .route('/monthly-plan/:year')
   .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan);
+
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
+
 router
   .route('/')
   .post(protect, restrictTo('admin', 'lead-guide'), createTour)
